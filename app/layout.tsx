332b914c608c33f168438802aa20a8f9978fe819
HaiDT <dotrihai1999@gmail.com>
2026-01-31 22:39:13 +0700

    feat: optimize metadata static/dynamic

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import Header from "@/components/header";
 import "./globals.css";
+import { Metadata } from "next";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "NextPosts",
   description: "Browse and share amazing posts.",
 };
